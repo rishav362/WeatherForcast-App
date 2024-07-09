bb3d21b8ae7a913a46171e8ee243da7a7b974f3f
Rishav Raj <rishavsinha362@gmail.com>
2024-07-09 22:55:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TimeAndLocation from "./components/TimeAndLocation.jsx";
 import TopButtons from "./components/TopButtons.jsx";
 import getFormattedWeatherData from "./services/weatherServices.js";
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () =>{
   const [query,setQuery] =useState({q:"tokyo" })
